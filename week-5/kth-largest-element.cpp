793b13f3fb892160987a5eb777d1361218545708
Vishal Choudhary <contactvishaltech@gmail.com>
2022-06-19 16:28:43 +0530

    kth-largest-pq

--- week-5/kth-largest-element.cpp
+++ week-5/kth-largest-element.cpp
@@ -1,14 +1,8 @@
 class Solution {
  public:
   int findKthLargest(vector<int>& nums, int k) {
-    vector<int> freq(20001, 0);
-    for (int i : nums) freq[10000 + i]++;
-    int ans, n = 20000;
-    while (k > 0) {
-      k -= freq[n];
-      ans = n - 10000;
-      n--;
-    }
-    return ans;
+    priority_queue<int> pq(nums.begin(), nums.end());
+    for (int i = 1; i < k; ++i) pq.pop();
+    return pq.top();
   }
 };
