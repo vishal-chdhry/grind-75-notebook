b6ecb0fe64918c54b79f8dabd147e1aa410dd27e
Vishal Choudhary <contactvishaltech@gmail.com>
2022-09-26 20:05:03 +0530

    myPow

---
+++ week-6/myPow.cpp
@@ -0,0 +1,12 @@
+class Solution {
+ public:
+  double myPow(double x, int n) {
+    double ans;
+    if (n == 0) return 1;
+    if (n & 1) {
+      return n > 0 ? x * pow(x, n - 1) : (1 / x) * pow(x, n + 1);
+    } else {
+      return myPow(x * x, n / 2);
+    }
+  }
+};
