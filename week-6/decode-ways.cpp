f81270eecda9fb335a6a0edab01451f42932aeaa
Vishal Choudhary <contactvishaltech@gmail.com>
2022-09-28 11:49:13 +0530

    decode-ways

--- week-6/decode-ways.cpp
+++ week-6/decode-ways.cpp
@@ -6,7 +6,7 @@ class Solution {
     v[0] = 1;
     for (int i = 1; i < s.size(); i++) {
       string a = s.substr(i - 1, 2);
-      v[i] += (stoi(a) <= 26 && stoi(a) >= 10 && s[i - 1] != '0')
+      v[i] += (a <= "26" && a >= "10" && s[i - 1] != '0')
                   ? (i - 2 >= 0 ? v[i - 2] : 1)
                   : 0;
       v[i] += (s[i] >= '1' && s[i] <= '9') ? v[i - 1] : 0;
