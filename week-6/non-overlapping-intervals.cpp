6c4bca9b1327cd3053cb27a4ff4a1274f91aad4e
Vishal Choudhary <contactvishaltech@gmail.com>
2022-10-01 23:26:38 +0530

    non-overlapping-intervals

---
+++ week-6/non-overlapping-intervals.cpp
@@ -0,0 +1,17 @@
+class Solution {
+ public:
+  int eraseOverlapIntervals(vector<vector<int>> &intervals) {
+    int ans = 0;
+    if (intervals.size() == 0) return 0;
+    sort(intervals.begin(), intervals.end(),
+         [](vector<int> &a1, vector<int> &a2) { return a1[1] < a2[1]; });
+    for (auto i = intervals.begin() + 1; i < intervals.end(); i++) {
+      if ((*i)[0] < (*(i - 1))[1]) {
+        ans++;
+        (*i)[1] = (*(i - 1))[1];
+      }
+    }
+    cout << endl;
+    return ans;
+  }
+};
