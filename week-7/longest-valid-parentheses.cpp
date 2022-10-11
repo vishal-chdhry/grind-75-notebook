98ec9c9003f100c30c4ddcc15a5a933fb29fbf37
Vishal Choudhary <contactvishaltech@gmail.com>
2022-10-11 21:05:16 +0530

    lvp improved
    Signed-off-by: Vishal Choudhary <contactvishaltech@gmail.com>

--- week-7/longest-valid-parentheses.cpp
+++ week-7/longest-valid-parentheses.cpp
@@ -3,18 +3,17 @@ class Solution {
   int longestValidParentheses(string s) {
     stack<int> st;
     st.push(-1);
-    int ans = 0, curr = 0;
+    int ans = 0;
     for (int i = 0; i < s.size(); i++) {
       curr++;
       if (s[i] == '(')
         st.push(i);
       else {
-        if (st.top() == -1) {
-          ans = max(ans, curr - 1);
-          curr = 0;
+        st.pop();
+        if (st.empty()) {
+          st.push(i);
         } else {
-          st.pop();
-          ans = max(ans, min(curr, (int)(i - st.top())));
+          ans = max(ans, (int)(i - st.top()));
         }
       }
     }
